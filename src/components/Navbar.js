9a403ea168ddce2f38f82a08407176b37e40c7aa
Harold Bernard <haroldbernard@Mayas-MacBook-Air.local>
2019-06-23 06:24:30 -0400

    fixed navbar route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import '../App.css';
 const Navbar = props => (
   <nav className="nav navbar navbar-expand-lg navbar-light bg-light nav-fill">
       <li className="nav-item">
-        <a href="/">{props.title}</a>
+        <a href="/click-and-react">{props.title}</a>
       </li>
 
       <li className="nav-item" id="rw">{props.rightWrong}</li>
