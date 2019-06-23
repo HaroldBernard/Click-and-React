051b77d74bae986bd716bc78f22b99da2847e331
Harold Bernard <haroldbernard@Mayas-MacBook-Air.local>
2019-06-23 04:28:29 -0400

    Updates

---
+++ precache-manifest.ec6cfb8ddb20f615e90552ad08756243.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da9ee819baebcdfa98e8a4783166501",
+    "url": "/HaroldBernard/click-and-react/index.html"
+  },
+  {
+    "revision": "6ec7c70665d22ca5e6ef",
+    "url": "/HaroldBernard/click-and-react/static/css/main.94cb6244.chunk.css"
+  },
+  {
+    "revision": "dcf5e17c5267f525fa95",
+    "url": "/HaroldBernard/click-and-react/static/js/2.7180d8fa.chunk.js"
+  },
+  {
+    "revision": "6ec7c70665d22ca5e6ef",
+    "url": "/HaroldBernard/click-and-react/static/js/main.019d0039.chunk.js"
+  },
+  {
+    "revision": "ac9dfd861ed0276eacef",
+    "url": "/HaroldBernard/click-and-react/static/js/runtime~main.44cea17c.js"
+  }
+]);
