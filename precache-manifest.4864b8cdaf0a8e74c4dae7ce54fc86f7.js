da0bab35a0211bed058ad69bf4cc537a5678adac
Harold Bernard <haroldbernard@Mayas-MacBook-Air.local>
2019-06-23 06:00:19 -0400

    Updates

---
+++ precache-manifest.4864b8cdaf0a8e74c4dae7ce54fc86f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ead23592f75d7f8227a135df6f1af4",
+    "url": "/click-and-react/index.html"
+  },
+  {
+    "revision": "160499e762152e5bc990",
+    "url": "/click-and-react/static/css/main.94cb6244.chunk.css"
+  },
+  {
+    "revision": "dcf5e17c5267f525fa95",
+    "url": "/click-and-react/static/js/2.7180d8fa.chunk.js"
+  },
+  {
+    "revision": "160499e762152e5bc990",
+    "url": "/click-and-react/static/js/main.950d9cad.chunk.js"
+  },
+  {
+    "revision": "180a065985b045f3cbb2",
+    "url": "/click-and-react/static/js/runtime~main.bdb133bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HaroldBernard/click-and-react/precache-manifest.ec6cfb8ddb20f615e90552ad08756243.js"
+  "/click-and-react/precache-manifest.4864b8cdaf0a8e74c4dae7ce54fc86f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HaroldBernard/click-and-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/click-and-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
